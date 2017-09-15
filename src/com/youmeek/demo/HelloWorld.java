36b8f3463dc58149bb1dae26f5fe2cf7d29e596c
RebertZhang <zhangbiao_9037@163.com>
2017-09-15 09:29:54 +0800

    idea 链接github 测试一下

---
+++ src/com/youmeek/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.youmeek.demo;
+
+/**
+ * Created by admin on 2017/9/15.
+ */
+public class HelloWorld {
+    public static  void  main(String[] args){
+        System.out.println("HelloWorld!");
+    }
+}
